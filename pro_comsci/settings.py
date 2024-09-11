07da5761b8e21c61cd2070bbcbeedfff666b4edd
deaww77 <126137481+deaww77@users.noreply.github.com>
2024-09-11 14:17:20 +0700

    fix setting

--- pro_comsci/settings.py
+++ pro_comsci/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URL = '/medai/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
